71a41231dce68594cd3f7bfc6b56e84bd0193f42
Tracie Masek <traciemasek@gmail.com>
2018-11-19 23:16:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    [array.push(`${musicians[i]} plays ${instruments[i]}`)];
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var excitedFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    excitedFacts.push(`${facts[i]}!!!`);
+    i++
+  }
+  return excitedFacts;
+}
